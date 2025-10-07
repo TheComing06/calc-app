2cac6693b1ea27849e425e5afa147dd7259a614e
TheComing06 <t.abdrahmanov999@gmail.com>
2025-10-07 14:16:40 +0700

    add button in main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import React, {useState} from "react";
 
 export default function HomePage() {
-  
 
   return (
     <>
@@ -12,12 +11,14 @@ export default function HomePage() {
           <h1 style={{ textAlign: 'center', margin: '20px', fontSize: '28px', fontWeight: 'bold' }}>Welcome To The Home Page</h1>
         </div>
       </header>
-      <main>
-        <div style={{ margin: '20px' }}>
-          <input className="rounded-md" style={{ background: 'oklch(92.9% 0.013 255.508)' }} type="text" />
-          <input className="rounded-md" style={{ background: 'oklch(92.9% 0.013 255.508)' }} type="text" />
+      <main className="flex flex-col items-center justify-center min-h-200">
+        <div className="text-black flex flex-col justify-between gap-5 rounded-xl border">
+          <input placeholder="Enter first number" className="rounded-md px-3 py-2" style={{ background: 'oklch(92.9% 0.013 255.508)', width: '300px' }} type="text" />
+          <input placeholder="Enter second number" type="text" className="rounded-md px-3 py-2" style={{ background: 'oklch(92.9% 0.013 255.508)', width: '300px' }}  />
         </div>
-        <button style={{  }}>Count</button>
+        <button className="m-10 rounded-md py-3 px-5 hover:shadow-amber-400 hover:shadow-xl hover:bg-yellow-400 
+        hover:text-black text-2xl font-extrabold bg-yellow-600
+        transition-all duration-1000">Count</button>
       </main>
     </>
   );
