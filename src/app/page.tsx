6bd75f65649d9c505bd0bce39c41bfe005b06750
TheComing06 <t.abdrahmanov999@gmail.com>
2025-10-09 10:29:10 +0700

    add Count function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,41 @@
 'use client'
 
 import React, {useState} from "react";
+import { eventNames } from "process";
 
 export default function HomePage() {
 
+  const [inputOne, setInputOne] = useState('');
+  const [inputTwo, setInputTwo] = useState('');
+
+  function Count(a:number, b:number) {
+    return a+b;
+  }
+
   return (
     <>
-      <header>
+      <header className="bg-amber-400 py-2">
         <div>
-          <h1 style={{ textAlign: 'center', margin: '20px', fontSize: '28px', fontWeight: 'bold' }}>Welcome To The Home Page</h1>
+          <h1 className="m-10 font-extrabold text-black" style={{fontSize: '48px', textAlign: 'center'}}>Welcome To The Calculator</h1>
         </div>
       </header>
       <main className="flex flex-col items-center justify-center min-h-200">
+        
+        <output className="bg-slate-200 w-60 px-4 py-2 flex flex-col text-neutral-500 rounded-md m-5">Answer:{Count(parseInt(inputOne), parseInt(inputTwo))}</output>
+
         <div className="text-black flex flex-col justify-between gap-5 rounded-xl border">
-          <input placeholder="Enter first number" className="rounded-md px-3 py-2" style={{ background: 'oklch(92.9% 0.013 255.508)', width: '300px' }} type="text" />
-          <input placeholder="Enter second number" type="text" className="rounded-md px-3 py-2" style={{ background: 'oklch(92.9% 0.013 255.508)', width: '300px' }}  />
+          <input name="input1" value={inputOne} onChange={(event) => setInputOne(event.target.value)} placeholder="Enter first number" className="rounded-md px-3 py-2 w-60 bg-slate-200" type="number" />
+          <input name="input2" value={inputTwo} onChange={(event) => setInputTwo(event.target.value)} placeholder="Enter second number" type="number" className="rounded-md px-3 py-2 bg-slate-200 w-60"  />
         </div>
+
         <button className="m-10 rounded-md py-3 px-5 hover:shadow-amber-400 hover:shadow-xl hover:bg-yellow-400 
         hover:text-black text-2xl font-extrabold bg-yellow-600
         transition-all duration-1000">Count</button>
+
       </main>
+      <footer>
+
+      </footer>
     </>
   );
 }
